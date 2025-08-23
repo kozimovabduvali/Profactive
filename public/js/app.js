8f654ad3243ae0edbf58a187304d0a95f70bda2c
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-08-23 18:05:35 +0500

    home page don

--- public/js/app.js
+++ public/js/app.js
@@ -7,19 +7,19 @@ let mainCatalogSwiper = new Swiper(".main-catalog__swiper", {
   breakpoints: {
     320: {
       slidesPerView: 1.3,
-      spaceBetween: 44,
+      spaceBetween: 20,
     },
     640: {
       slidesPerView: 1.8,
-      spaceBetween: 44,
+      spaceBetween: 25,
     },
     768: {
       slidesPerView: 2.5,
-      spaceBetween: 44,
+      spaceBetween: 30,
     },
     1024: {
       slidesPerView: 3.5,
-      spaceBetween: 44,
+      spaceBetween: 35,
     },
     1280: {
       slidesPerView: 4,
