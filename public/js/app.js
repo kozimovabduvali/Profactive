ac82eb34eddfc6db1fda80da02c5cf098595619a
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-08-23 06:15:33 +0500

    Каталог курсов section done

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+let mainCatalogSwiper = new Swiper(".main-catalog__swiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 44,
+    },
+    640: {
+      slidesPerView: 1.8,
+      spaceBetween: 44,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 44,
+    },
+    1024: {
+      slidesPerView: 3.5,
+      spaceBetween: 44,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 44,
+    }
+  },
+});
